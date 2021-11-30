e9474aaa8da6f86e9b40eaa918289e5ef9df7aa9
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-11-30 10:00:48 +0100

    Exempel 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.gbxLäggTill = new System.Windows.Forms.GroupBox();
-            this.tbxLäggTill = new System.Windows.Forms.TextBox();
             this.btnLäggTill = new System.Windows.Forms.Button();
+            this.tbxLäggTill = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblLista = new System.Windows.Forms.Label();
+            this.gbxKollaVärde = new System.Windows.Forms.GroupBox();
+            this.btnKollaVärde = new System.Windows.Forms.Button();
+            this.tbxKollaVärde = new System.Windows.Forms.TextBox();
             this.gbxLäggTill.SuspendLayout();
+            this.gbxKollaVärde.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbxLäggTill
@@ -45,14 +49,7 @@
             this.gbxLäggTill.Size = new System.Drawing.Size(200, 100);
             this.gbxLäggTill.TabIndex = 0;
             this.gbxLäggTill.TabStop = false;
-            this.gbxLäggTill.Text = "groupBox1";
-            // 
-            // tbxLäggTill
-            // 
-            this.tbxLäggTill.Location = new System.Drawing.Point(16, 31);
-            this.tbxLäggTill.Name = "tbxLäggTill";
-            this.tbxLäggTill.Size = new System.Drawing.Size(100, 22);
-            this.tbxLäggTill.TabIndex = 0;
+            this.gbxLäggTill.Text = "Lägg till";
             // 
             // btnLäggTill
             // 
@@ -62,37 +59,76 @@
             this.btnLäggTill.TabIndex = 1;
             this.btnLäggTill.Text = "Lägg till";
             this.btnLäggTill.UseVisualStyleBackColor = true;
+            this.btnLäggTill.Click += new System.EventHandler(this.btnLäggTill_Click);
+            // 
+            // tbxLäggTill
+            // 
+            this.tbxLäggTill.Location = new System.Drawing.Point(16, 31);
+            this.tbxLäggTill.Name = "tbxLäggTill";
+            this.tbxLäggTill.Size = new System.Drawing.Size(100, 22);
+            this.tbxLäggTill.TabIndex = 0;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(349, 53);
+            this.listBox1.Location = new System.Drawing.Point(561, 53);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 84);
             this.listBox1.TabIndex = 1;
             // 
-            // label1
+            // lblLista
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(346, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.lblLista.AutoSize = true;
+            this.lblLista.Location = new System.Drawing.Point(346, 22);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(46, 17);
+            this.lblLista.TabIndex = 2;
+            this.lblLista.Text = "label1";
+            // 
+            // gbxKollaVärde
+            // 
+            this.gbxKollaVärde.Controls.Add(this.btnKollaVärde);
+            this.gbxKollaVärde.Controls.Add(this.tbxKollaVärde);
+            this.gbxKollaVärde.Location = new System.Drawing.Point(26, 140);
+            this.gbxKollaVärde.Name = "gbxKollaVärde";
+            this.gbxKollaVärde.Size = new System.Drawing.Size(200, 109);
+            this.gbxKollaVärde.TabIndex = 3;
+            this.gbxKollaVärde.TabStop = false;
+            this.gbxKollaVärde.Text = "Se om värde finns";
+            // 
+            // btnKollaVärde
+            // 
+            this.btnKollaVärde.Location = new System.Drawing.Point(78, 60);
+            this.btnKollaVärde.Name = "btnKollaVärde";
+            this.btnKollaVärde.Size = new System.Drawing.Size(75, 23);
+            this.btnKollaVärde.TabIndex = 1;
+            this.btnKollaVärde.Text = "Kolla värde";
+            this.btnKollaVärde.UseVisualStyleBackColor = true;
+            this.btnKollaVärde.Click += new System.EventHandler(this.btnKollaVärde_Click);
+            // 
+            // tbxKollaVärde
+            // 
+            this.tbxKollaVärde.Location = new System.Drawing.Point(16, 31);
+            this.tbxKollaVärde.Name = "tbxKollaVärde";
+            this.tbxKollaVärde.Size = new System.Drawing.Size(100, 22);
+            this.tbxKollaVärde.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbxKollaVärde);
+            this.Controls.Add(this.lblLista);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.gbxLäggTill);
             this.Name = "Form1";
             this.Text = "Form1";
             this.gbxLäggTill.ResumeLayout(false);
             this.gbxLäggTill.PerformLayout();
+            this.gbxKollaVärde.ResumeLayout(false);
+            this.gbxKollaVärde.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,7 +140,10 @@
         private System.Windows.Forms.Button btnLäggTill;
         private System.Windows.Forms.TextBox tbxLäggTill;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblLista;
+        private System.Windows.Forms.GroupBox gbxKollaVärde;
+        private System.Windows.Forms.Button btnKollaVärde;
+        private System.Windows.Forms.TextBox tbxKollaVärde;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,53 @@ namespace SamlingsKlasserEx1
 {
     public partial class Form1 : Form
     {
+        //Skapa en lista av int
+        List<int> lista = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLäggTill_Click(object sender, EventArgs e)
+        {
+
+            //Om värdet är av typen int ska det läggas till i listan
+            try {
+                lista.Add(int.Parse(tbxLäggTill.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Felaktig inmatning!", "Caption");
+            }
+            //Skriv ut listan
+            FyllListan();
+        }
+
+        private void FyllListan()
+        {
+            //Nollställ labeln lblList
+            lblLista.Text = "";
+
+            //Fyll listboxen med innehållet från listan
+            for(int i = 0; i < lista.Count; i++)
+            {
+                lblLista.Text += lista[i] + "\r\n";
+            }
+        }
+
+        private void btnKollaVärde_Click(object sender, EventArgs e)
+        {
+            //Kolla om värdet i textboxen finns i listan
+            try
+            {
+                lblLista.Text = "Talet " + int.Parse(tbxKollaVärde.Text)
+                    + " finns i listan " + lista.Contains(int.Parse(tbxKollaVärde.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Felaktig inmatning!", "Caption");
+            }
         }
     }
 }
